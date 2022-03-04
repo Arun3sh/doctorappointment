f681b0ec8b464665d10d249bc88ee3992bd5b5a0
Arun3sh <m.aruneshwar@gmail.com>
2022-03-04 23:42:13 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
 
 function App() {
-	return <div className="App">Hospital App Initialised</div>;
+	return (
+		<div className="App container-sm">
+			<Navigation />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/**">Error</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
