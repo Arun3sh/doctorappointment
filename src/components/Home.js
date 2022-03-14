7286225bde4f67b0cf4e10a8a74b1aa79c3609e9
Arun3sh <m.aruneshwar@gmail.com>
2022-03-14 23:46:29 +0530

    testimonial added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import Header from './Header';
-import { Card, CardMedia, CardActions, CardContent, Typography, Button } from '@mui/material';
+import {
+	Card,
+	CardMedia,
+	CardActions,
+	CardContent,
+	Typography,
+	Button,
+	Avatar,
+} from '@mui/material';
+import Slider from 'react-touch-drag-slider';
 
 function Home() {
 	// const myStyle = { backgroundImage: `${imgData[index]}`, height: '350px' };
@@ -24,6 +33,13 @@ function Home() {
 				<h3>DELIVERING IMPACTFUL SOLUTIONS</h3>
 
 				<Solution />
+			</div>
+
+			{/* Testimonials */}
+			<div className="testimonial">
+				<h3 className="heading-center">TESTIMONIALS</h3>
+
+				<Testimonial />
 			</div>
 		</div>
 	);
@@ -68,4 +84,71 @@ function Solution() {
 		</div>
 	);
 }
+
+function Testimonial() {
+	const testimonialData = [
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Corporate Health Centre has totally changed my outlook towards my personal health and wellbeing. Now, no more of “I don’t have time” excuses. Thanks, team',
+		},
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Est cillum ea dolor amet. Nulla culpa incididunt dolor incididunt nostrud officia ex aliqua in duis sint veniam. Eiusmod excepteur eiusmod qui dolore nisi commodo.',
+		},
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Est cillum ea dolor amet. Nulla culpa incididunt dolor incididunt nostrud officia ex aliqua in duis sint veniam. Eiusmod excepteur eiusmod qui dolore nisi commodo.',
+		},
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Est cillum ea dolor amet. Nulla culpa incididunt dolor incididunt nostrud officia ex aliqua in duis sint veniam. Eiusmod excepteur eiusmod qui dolore nisi commodo.',
+		},
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Est cillum ea dolor amet. Nulla culpa incididunt dolor incididunt nostrud officia ex aliqua in duis sint veniam. Eiusmod excepteur eiusmod qui dolore nisi commodo.',
+		},
+		{
+			name: 'Patient Name',
+			position: 'Place/ Position',
+			userOpinion:
+				'Est cillum ea dolor amet. Nulla culpa incididunt dolor incididunt nostrud officia ex aliqua in duis sint veniam. Eiusmod excepteur eiusmod qui dolore nisi commodo.',
+		},
+	];
+	return (
+		<div className="testimonial-wrapper">
+			<Slider
+				className="slider-testimonial"
+				activeIndex={0}
+				threshHold={100}
+				transition={0.5}
+				scaleOnDrag={false}
+			>
+				{testimonialData.map(({ name, position, userOpinion }, index) => (
+					<Card sx={{ maxWidth: 320 }} className="testimonial-card" key={index}>
+						{/* <CardMedia component="img" height="240" image={img} alt={title} /> */}
+						<CardContent className="slider-cardContent">
+							<Avatar className="avatar-user" src="/broken-image.jpg" />
+							<Typography className="userOpinion" variant="p">
+								{userOpinion}
+							</Typography>
+							<Typography variant="h5">{name}</Typography>
+							<Typography variant="p">{position}</Typography>
+						</CardContent>
+					</Card>
+				))}
+			</Slider>
+		</div>
+	);
+}
+
 export default Home;
