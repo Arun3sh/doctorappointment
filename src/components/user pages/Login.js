219b6ad8dd3eb3c5555ba15782f19eeb1a151621
Arun3sh <m.aruneshwar@gmail.com>
2022-04-09 23:42:43 +0530

    updated

--- src/components/user pages/Login.js
+++ src/components/user pages/Login.js
@@ -1,4 +1,4 @@
-import './../user pages/Login.css';
+import './Login.css';
 import docIllu from '../../asset/images/doctor-in-phone.png';
 import {
 	Button,
@@ -10,8 +10,11 @@ import {
 	TextField,
 } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+import { authContext } from '../../App';
 
 function Login() {
+	const { login, setLogin } = useContext(authContext);
 	const history = useHistory();
 	return (
 		<div className="container-sm login-wrapper">
@@ -24,31 +27,35 @@ function Login() {
 				<img className="login-img" src={docIllu} aria-label="user illustration" alt="broken" />
 				{/* </div> */}
 				<div className="login-container-form">
-					<form className="login-form">
-						{/* select radio options for doctor patient login */}
-						<FormControl>
-							<RadioGroup
-								className="radioGroup-userType"
-								aria-label="type of user"
-								defaultValue="patient"
-								name="type-user"
-							>
-								<FormControlLabel value="patient" control={<Radio />} label="Patient" />
-								<FormControlLabel value="doctor" control={<Radio />} label="Doctor" />
-							</RadioGroup>
-						</FormControl>
+					<Paper elevation={3} style={{ padding: '25px' }}>
+						<form className="login-form">
+							{/* select radio options for doctor patient login */}
+							<FormControl>
+								<RadioGroup
+									className="radioGroup-userType"
+									aria-label="type of user"
+									defaultValue="patient"
+									name="type-user"
+								>
+									<FormControlLabel value="patient" control={<Radio />} label="Patient" />
+									<FormControlLabel value="doctor" control={<Radio />} label="Doctor" />
+								</RadioGroup>
+							</FormControl>
 
-						<div className="login-form-div">
-							<TextField variant="standard" label="email id" autoFocus />
-							<TextField variant="standard" label="password" type="password" />
-							<div className="submit-user">
-								<Button variant="outlined" color="error" onClick={() => history.goBack()}>
-									Back
-								</Button>
-								<Button variant="outlined">Submit</Button>
+							<div className="login-form-div">
+								<TextField variant="standard" label="email id" autoFocus />
+								<TextField variant="standard" label="password" type="password" />
+								<div className="submit-user">
+									<Button variant="outlined" color="error" onClick={() => history.goBack()}>
+										Back
+									</Button>
+									<Button variant="outlined" onClick={() => setLogin(!login) & history.push('/')}>
+										Submit
+									</Button>
+								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</Paper>
 				</div>
 			</div>
 		</div>
