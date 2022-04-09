76d939889ab436da04664e6f62542c82d5d68d6a
Arun3sh <m.aruneshwar@gmail.com>
2022-04-09 23:43:04 +0530

    based on login changes

--- src/App.js
+++ src/App.js
@@ -4,33 +4,42 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Login from './components/user pages/Login';
 import Appointment from './components/user pages/Appointment';
+
+export const authContext = createContext(null);
 
 function App() {
 	const [login, setLogin] = useState(false);
 	const location = useLocation();
 
+	const variables = {
+		login: login,
+		setLogin: setLogin,
+	};
+
 	return (
-		<div className="App">
-			{location.pathname === '/register' || location.pathname === '/login' ? '' : <Navigation />}
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/register">register</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/appointment">
-					<Appointment />
-				</Route>
-				<Route path="/**">Error</Route>
-			</Switch>
-			{location.pathname === '/register' || location.pathname === '/login' ? '' : <Footer />}
-		</div>
+		<authContext.Provider value={variables}>
+			<div className="App">
+				{location.pathname === '/register' || location.pathname === '/login' ? '' : <Navigation />}
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/register">register</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/appointment">
+						<Appointment />
+					</Route>
+					<Route path="/**">Error</Route>
+				</Switch>
+				{location.pathname === '/register' || location.pathname === '/login' ? '' : <Footer />}
+			</div>
+		</authContext.Provider>
 	);
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,12 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+import { authContext } from '../App';
 
 function Navigation() {
+	const { login, setLogin } = useContext(authContext);
 	const history = useHistory();
 
 	return (
@@ -31,27 +34,59 @@ function Navigation() {
 						<Link className="nav-link" to="/solution">
 							Solution
 						</Link>
+
+						{/* Once user is logged in they can see this specific menu to help them navigate to medical space */}
+						{login ? (
+							<NavDropdown title="Medical Records" id="basic-nav-dropdown">
+								<NavDropdown.Item as={Link} to="/appointment">
+									Appointment
+								</NavDropdown.Item>
+								<NavDropdown.Item as={Link} to="/treatment-record">
+									Treatment Record
+								</NavDropdown.Item>
+								<NavDropdown.Item as={Link} to="/prescription">
+									Prescription
+								</NavDropdown.Item>
+							</NavDropdown>
+						) : (
+							''
+						)}
 					</Nav>
 
-					{/* Login Button */}
-					<Button
-						className="login-btn"
-						variant="text"
-						color="primary"
-						onClick={() => history.push('/login')}
-					>
-						Login
-					</Button>
+					{/* Login & Logout Button */}
+					{!login ? (
+						<Button
+							className="login-btn"
+							variant="text"
+							color="primary"
+							onClick={() => history.push('/login')}
+						>
+							Login
+						</Button>
+					) : (
+						<Button
+							className="login-btn"
+							variant="text"
+							color="primary"
+							onClick={() => setLogin(!login) & history.push('/')}
+						>
+							Logout
+						</Button>
+					)}
 
-					{/* Signup Button */}
-					<Button
-						className="signup-btn"
-						variant="contained"
-						color="success"
-						onClick={() => history.push('/register')}
-					>
-						Sign up
-					</Button>
+					{/* Signup Button & User avatar after login*/}
+					{!login ? (
+						<Button
+							className="signup-btn"
+							variant="contained"
+							color="success"
+							onClick={() => history.push('/register')}
+						>
+							Sign up
+						</Button>
+					) : (
+						<Avatar src="/broken-image.jpg" />
+					)}
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
